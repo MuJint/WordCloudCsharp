8d1a050437223df587e8e0f98ffd0f50b3637ac3
yuxin <yuxin.bb@qq.com>
2022-06-27 14:44:45 +0800

    fix：清理项目

--- ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Drawing.Imaging;
-using WordCloudCsharp;
-
-Console.WriteLine("Hello, World!");
-using var images = ISingletion<WordcloudSrv>.Instance
-                     .GetWordCloud(4000, 4000)
-                     .Draw(new List<string>() { "Sign", "Xia" }, new List<int>() { 1, 2 });
-images.Save($"D:\\{Guid.NewGuid()}.png", ImageFormat.Png);
