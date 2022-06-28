1b96beaff7bf553f616adaaec78af0a68b6f5665
yuxin <yuxin.bb@qq.com>
2022-06-28 11:46:19 +0800

    fix：更新nuget包。更新测试

--- README.md
+++ README.md
@@ -12,24 +12,39 @@ This project is ported from a Codeplex hosted repository.
 
 * [orig author](http://www.codeplex.com/site/users/view/briancullen)
 
-# Build
-
-![GitHub Actions]()
-
 # Install
 
 [![NuGet](https://img.shields.io/nuget/v/WordCloudSharp.svg)]()
 
 # Usage
-
+* NameSpace ?命名空间
+```
+    using WordCloudCsharp;
+```
+* Singletion ?自带单例
 ```
     using var images = ISingletion<WordcloudSrv>.Instance
                      .GetWordCloud(4000, 4000)
                      .Draw(new List<string>() { "Sign", "Xia" }, new List<int>() { 1, 2 });
     images.Save($"D:\\{Guid.NewGuid()}.png", ImageFormat.Png);
 ```
+* IOC ?IOC使用
+```
+    //di  ?注入
+    services.AddSingleton<IWordcloud, WordcloudSrv>();
 
-for more details, please ref to the usage in UnitTest1
+    //ctor ?构造函数注入
+    readonly IWordcloud _wordcloud;
+    public XXX(IWordcloud wordcloud)
+    {
+        _wordcloud = wordcloud;
+    }
+    
+    using var images = _wordcloud.GetWordCloud(4000, 4000)
+                     .Draw(new List<string>() { "Sign", "Xia" }, new List<int>() { 1, 2 });
+    images.Save($"D:\\{Guid.NewGuid()}.png", ImageFormat.Png);
+```
+for more details, please ref to the usage in UnitTest1. ?更多使用用例请查看 UnitTest1
 
 # Examples
 
--- test/TestProject/UnitTest1.cs
+++ test/TestProject/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using System.Drawing.Imaging;
 using WordCloudCsharp;
 
@@ -5,8 +6,11 @@ namespace TestProject
 {
     public class UnitTest1
     {
+        readonly List<string> words = new() { "Sign", "Xia", "张三2", "签到", "在吗", "上网啊" };
+        readonly List<int> feq = new() { 1, 2, 1, 1, 1, 1 };
+
         [Fact]
-        public void Test1()
+        public void TestInteration()
         {
             try
             {
@@ -22,6 +26,72 @@ namespace TestProject
             {
                 throw;
             }
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            using var images2 = ISingletion<WordcloudSrv>.Instance
+                 .GetWordCloud(200, 200, useRank: true)
+                 .Draw(words, feq);
+            images2.Save($"D:\\foreach\\{Guid.NewGuid()}.png", ImageFormat.Png);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            using var images2 = ISingletion<WordcloudSrv>.Instance
+                 .GetWordCloud(200, 200, useRank: true, fontColor: Color.AliceBlue)
+                 .Draw(words, feq);
+            images2.Save($"D:\\foreach\\{Guid.NewGuid()}.png", ImageFormat.Png);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            using var images2 = ISingletion<WordcloudSrv>.Instance
+                 .GetWordCloud(500, 400,fontColor:Color.Black,maxFontSize:24)
+                 .Draw(words, feq);
+            images2.Save($"D:\\foreach\\{Guid.NewGuid()}.png", ImageFormat.Png);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            using var images2 = ISingletion<WordcloudSrv>.Instance
+                 .GetWordCloud(500, 400, fontColor: Color.Black, fontStep: 5)
+                 .Draw(words, feq);
+            images2.Save($"D:\\foreach\\{Guid.NewGuid()}.png", ImageFormat.Png);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            using var images2 = ISingletion<WordcloudSrv>.Instance
+                 .GetWordCloud(500, 400, fontColor: Color.DarkRed, allowVerical: true)
+                 .Draw(words, feq);
+            images2.Save($"D:\\foreach\\{Guid.NewGuid()}.png", ImageFormat.Png);
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            using var images2 = ISingletion<WordcloudSrv>.Instance
+                 .GetWordCloud(500, 400, fontColor: Color.DarkRed,fontname:"fangsong")
+                 .Draw(words, feq);
+            images2.Save($"D:\\foreach\\{Guid.NewGuid()}.png", ImageFormat.Png);
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            //imgMark need vaild black/white  ? 需要有效的黑或白蒙板图片
+            using var fileStream = new FileStream(@"C:\Users\86152\Desktop\1-21061PUH3.jpg", FileMode.Open);
+            var img = Image.FromStream(fileStream);
+            using var images2 = ISingletion<WordcloudSrv>.Instance
+                 .GetWordCloud(580, 387, fontColor: Color.Pink, fontname: "kaiti", mask: img)
+                 .Draw(words, feq);
+            images2.Save($"D:\\foreach\\{Guid.NewGuid()}.png", ImageFormat.Png);
         }
     }
 }
